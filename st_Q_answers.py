1ffb3e303b323a51d0b372fe79be876eb103692c
yotam hadari <yotamhh@gmail.com>
2022-07-17 09:53:41 +0300

    updates to week management

--- st_Q_answers.py
+++ st_Q_answers.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from itertools import dropwhile
 from unittest import skip
 import streamlit as st
 import pandas as pd
@@ -34,22 +36,17 @@ df = pd.read_csv(
 df.tail()
 
 df["Week"] = np.nan
+week_count = 1
+day_count = 1
 for n in range(0, len(df)):
     date_bool = (
-        df.index.get_level_values(1)[n] - df.index.get_level_values(1)[n - 1]) == 3
-    week_count = 1
-    day_count = 1
+        df.index.get_level_values(1)[n] - df.index.get_level_values(1)[n - 1]) >= timedelta(3)
     match date_bool:
         case True:  ## This Section handles creating a "week in trial" column by using the dates given a part of the index
             week_count += 1  ## Sunday is the first day of the week!!!!
             df.iloc[n, -1] = week_count
         case False:
-            day_count += 1
-            if day_count == 8:
-                week_count += 1
-                day_count = 1
             df.iloc[n, -1] = week_count
-
 
 def create_indiviudal_user_dfs(df):
     """Create a dictionary where keys are user_n and values and individual answer df for that user."""
@@ -95,8 +92,9 @@ def bar_graph_data(df, user_n="1", base_line=None):
         col1.subheader("Missing Baseline CSV file")
         pass
 
+    #st.write(user)
     grouped_week = user.groupby(
-        "Week"
+        "Week", dropna=False
     ).sum()  # Preping dataframe for weekly answers visualization
     grouped_week.drop("Pain VAS", axis=1, inplace=True)
 
