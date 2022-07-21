fa14e588a599c13b0e4ee93db51e9f16ddd2dc35
yotam hadari <yotamhh@gmail.com>
2022-07-21 18:39:04 +0300

    Formatting

--- st_users_answer_compare.py
+++ st_users_answer_compare.py
@@ -33,18 +33,6 @@ def question_comprasion(df):
     radio = st.radio(
         'Which question would you like to compare between USERS?',
         df.columns)
-
-    users_dfs = create_indiviudal_user_dfs(df) # dictionary
-    users_vas = {}
-    for user in users_dfs.keys():
-        user_ts = users_dfs[user]
-        user_ts = user_ts[radio]
-        users_vas[user] = user_ts
-        
-
-    vas_df = pd.DataFrame(users_vas)
-    vas_df = vas_df.groupby(vas_df.index.get_level_values(1)).sum()
-    vas_df.set_index(vas_df.index.date)
     
     fig = px.line(df.reset_index(), x='Date', y=radio, color='User ID', line_dash='User ID', markers=True,)
     st.plotly_chart(fig, use_container_width=True)
